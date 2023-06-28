cf72a2de476ab1ce9decee89075d28239d173afe
Jim C OG <127059791+Leviathon777@users.noreply.github.com>
2023-06-28 01:02:04 +0000

    Update NFTCardTwo.jsx

--- collectionPage/NFTCardTwo/NFTCardTwo.jsx
+++ collectionPage/NFTCardTwo/NFTCardTwo.jsx
@@ -1,18 +1,21 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import Style from "./NFTCardTwo.module.css";
 import Rating from "react-rating";
 import { FaRegStar, FaStar } from "react-icons/fa";
 import images from "../../img";
 import Image from "next/image";
-
-import { doc, updateDoc, getFirestore, getDoc, setDoc } from "firebase/firestore";
-// cc
-import ReactPlayer from 'react-player';
-import { LazyLoadComponent } from 'react-lazy-load-image-component';
+import {
+  doc,
+  updateDoc,
+  getFirestore,
+  getDoc,
+  setDoc,
+} from "firebase/firestore";
+import ReactPlayer from "react-player";
+import { LazyLoadComponent } from "react-lazy-load-image-component";
 import { LazyLoadImage } from "react-lazy-load-image-component";
 import "react-lazy-load-image-component/src/effects/blur.css";
 import { useAddress } from "@thirdweb-dev/react";
-
 import Link from "next/link";
 
 const NFTCardTwo = ({ NFTData }) => {
@@ -20,291 +23,211 @@ const NFTCardTwo = ({ NFTData }) => {
   const ITEMS_PER_PAGE = 12;
   const [fileTypes, setFileTypes] = useState({});
   const [loading, setLoading] = useState(true);
-  const [ratings, setRatings] = useState({});
+  const [likes, setLikes] = useState(() => {
+    if (typeof window !== "undefined") {
+      const savedLikes = localStorage.getItem("nftLikes");
+      return savedLikes ? JSON.parse(savedLikes) : {};
+    }
+    return {};
+  });
+
   const firestore = getFirestore();
   const walletAddress = useAddress();
 
-const [likes, setLikes] = useState(() => {
-  if (typeof window !== 'undefined') {
-    const savedLikes = localStorage.getItem("nftLikes");
-    return savedLikes ? JSON.parse(savedLikes) : {};
-  }
-  return {};
-});
-
-
-/*
-useEffect(() => {
-    const fetchRatings = () => {
-      NFTData.forEach((nft) => {
-        const nftRef = doc(firestore, "likes", nft.tokenId); 
-        onSnapshot(nftRef, (snapshot) => {
-          if (snapshot.exists()) {
-            const data = snapshot.data();
-            setRatings((prevRatings) => ({
-              ...prevRatings,
-              [nft.tokenId]: data.rating || 0,
-            }));
-          }
-        });
-      });
-    };
-
-    fetchRatings();
-  }, [NFTData]);
-*/
-
-/*
   useEffect(() => {
-  const fetchLikesAndRatings = async () => {
-    for (const nft of NFTData) {
-      const nftRef = doc(firestore, "likes", nft.tokenId);
-      const snapshot = await getDoc(nftRef);
-      if (snapshot.exists()) {
-        const data = snapshot.data();
-        setLikes((prevLikes) => ({
-          ...prevLikes,
-          [nft.tokenId]: {
-            count: data.count || 0,
-            rating: data.rating || 0,
-            liked: prevLikes[nft.tokenId]
-              ? prevLikes[nft.tokenId].liked
-              : false,
-          },
-        }));
-      }
-    }
-  };
+    const fetchLikesAndRatings = async () => {
+      let likesObj = {};
 
-  fetchLikesAndRatings();
-}, [NFTData]);
-*/
+      // make the list so not getting them over and over
+      const promises = NFTData.map((nft) => {
+        const nftRef = doc(firestore, "likes", nft.tokenId);
+        return getDoc(nftRef);
+      });
 
-useEffect(() => {
-  const fetchLikesAndRatings = async () => {
-    for (const nft of NFTData) {
-      const nftRef = doc(firestore, "likes", nft.tokenId);
-      const snapshot = await getDoc(nftRef);
-      if (snapshot.exists()) {
-        const data = snapshot.data();
-        setLikes((prevLikes) => ({
-          ...prevLikes,
-          [nft.tokenId]: {
+      // promise.all() to wait itl they all resolve
+      const snapshots = await Promise.all(promises);
+
+      snapshots.forEach((snapshot, index) => {
+        const nft = NFTData[index];
+        if (snapshot.exists()) {
+          const data = snapshot.data();
+          likesObj[nft.tokenId] = {
             count: data.count || 0,
             rating: data.rating || 0,
             liked: data.wallets && data.wallets[walletAddress] ? true : false,
             wallets: data.wallets || {},
-          },
-        }));
-      }
-    }
-  };
+          };
+        }
+      });
 
-  fetchLikesAndRatings();
-}, [NFTData, walletAddress]);
-
-
-
-/*
-const likeNFT = async (tokenId, ratingValue, walletAddress) => {
-  setLikes((prevState) => {
-    const newLikes = { ...prevState };
-    if (!newLikes[tokenId]) {
-      newLikes[tokenId] = { count: 0, liked: false, rating: 0 };
-    }
-    newLikes[tokenId].liked = !newLikes[tokenId].liked;
-    if (newLikes[tokenId].liked) {
-      newLikes[tokenId].count++;
-      newLikes[tokenId].rating = ratingValue;
-    } else {
-      newLikes[tokenId].count--;
-      newLikes[tokenId].rating = 0;
-    }
-
-    if (typeof window !== "undefined") {
-      localStorage.setItem("nftLikes", JSON.stringify(newLikes));
-    }
-
-    // Store likes and rating in Firebase Firestore by tokenId
-    const updateLikesAndRating = async () => {
-      const likesRef = doc(firestore, "likes", tokenId);
-      const likesSnapshot = await getDoc(likesRef);
-      if (likesSnapshot.exists()) {
-        // Update existing document
-        await updateDoc(likesRef, {
-          count: newLikes[tokenId].count,
-          rating: newLikes[tokenId].rating,
-        });
-      } else {
-        // Create new document
-        await setDoc(likesRef, {
-          count: newLikes[tokenId].count,
-          rating: newLikes[tokenId].rating,
-        });
-      }
+      setLikes(likesObj);
     };
 
-    updateLikesAndRating();
+    fetchLikesAndRatings();
+  }, [NFTData, walletAddress]);
 
-    return newLikes;
-  });
-};
-*/
+  const likeNFT = useCallback(
+    async (tokenId, ratingValue, walletAddress) => {
+      setLikes((prevState) => {
+        const newLikes = { ...prevState };
+        if (!newLikes[tokenId]) {
+          newLikes[tokenId] = {
+            count: 0,
+            liked: false,
+            rating: 0,
+            wallets: {},
+          };
+        }
 
+        // check if wallets property exists and is an object
+        if (
+          !newLikes[tokenId].wallets ||
+          typeof newLikes[tokenId].wallets !== "object"
+        ) {
+          newLikes[tokenId].wallets = {};
+        }
 
-const likeNFT = async (tokenId, ratingValue, walletAddress) => {
-  setLikes((prevState) => {
-    const newLikes = { ...prevState };
-    if (!newLikes[tokenId]) {
-      newLikes[tokenId] = { count: 0, liked: false, rating: 0, wallets: {} };
-    }
-    // Check if this wallet has already liked/rated this NFT
-    if (newLikes[tokenId].wallets[walletAddress]) {
-      newLikes[tokenId].liked = false;
-      newLikes[tokenId].count--;
-      //newLikes[tokenId].rating = 0;
-      delete newLikes[tokenId].wallets[walletAddress];
-    } else {
-      newLikes[tokenId].liked = true;
-      newLikes[tokenId].count++;
-      newLikes[tokenId].rating = ratingValue;
-      newLikes[tokenId].wallets[walletAddress] = true;
-    }
+        // check if this wallet has already liked/rated
+        if (newLikes[tokenId].wallets[walletAddress]) {
+          newLikes[tokenId].liked = false;
+          newLikes[tokenId].count--;
+          delete newLikes[tokenId].wallets[walletAddress];
+        } else {
+          newLikes[tokenId].liked = true;
+          newLikes[tokenId].count++;
+          newLikes[tokenId].rating = ratingValue;
+          newLikes[tokenId].wallets[walletAddress] = true;
+        }
 
-    if (typeof window !== "undefined") {
-      localStorage.setItem("nftLikes", JSON.stringify(newLikes));
-    }
+        if (typeof window !== "undefined") {
+          localStorage.setItem("nftLikes", JSON.stringify(newLikes));
+        }
 
-    // Store likes and rating in Firebase Firestore by tokenId
-    const updateLikesAndRating = async () => {
-      const likesRef = doc(firestore, "likes", tokenId);
-      const likesSnapshot = await getDoc(likesRef);
-      if (likesSnapshot.exists()) {
-        // Update existing document
-        await updateDoc(likesRef, {
-          count: newLikes[tokenId].count,
-          rating: newLikes[tokenId].rating,
-          wallets: newLikes[tokenId].wallets,
-        });
-      } else {
-        // Create new document
-        await setDoc(likesRef, {
-          count: newLikes[tokenId].count,
-          rating: newLikes[tokenId].rating,
-          wallets: newLikes[tokenId].wallets,
-        });
+        // Store likes and rating in Firebase Firestore by tokenId
+        const updateLikesAndRating = async () => {
+          const likesRef = doc(firestore, "likes", tokenId);
+          const likesSnapshot = await getDoc(likesRef);
+          if (likesSnapshot.exists()) {
+            await updateDoc(likesRef, {
+              count: newLikes[tokenId].count,
+              rating: newLikes[tokenId].rating,
+              wallets: newLikes[tokenId].wallets,
+            });
+          } else {
+            await setDoc(likesRef, {
+              count: newLikes[tokenId].count,
+              rating: newLikes[tokenId].rating,
+              wallets: newLikes[tokenId].wallets,
+            });
+          }
+        };
+
+        updateLikesAndRating();
+
+        return newLikes;
+      });
+    },
+    [firestore, walletAddress]
+  );
+
+  useEffect(() => {
+    const fetchFileTypes = async () => {
+      let fileTypesObj = {};
+
+      const savedData = localStorage.getItem("fileTypesObj");
+      if (savedData) {
+        fileTypesObj = JSON.parse(savedData);
       }
-    };
 
-    updateLikesAndRating();
-
-    return newLikes;
-  });
-};
-
-
-
-useEffect(() => {
-  const fetchFileTypes = async () => {
-    let fileTypesObj = {};
-
-    const savedData = localStorage.getItem('fileTypesObj');
-    if (savedData) {
-      fileTypesObj = JSON.parse(savedData);
-    }
-
-    for (const el of NFTData) {
-      if (!fileTypesObj[el.image]) {
-        try {
-          const response = await fetch(el.image);
-          const contentType = response.headers.get("content-type");
-          fileTypesObj[el.image] = contentType;
-        } catch (error) {
-          console.log(error);
+      for (const el of NFTData) {
+        if (!fileTypesObj[el.image]) {
+          try {
+            const response = await fetch(el.image);
+            const contentType = response.headers.get("content-type");
+            fileTypesObj[el.image] = contentType;
+          } catch (error) {
+            console.log(error);
+          }
         }
       }
-    }
 
-    localStorage.setItem('fileTypesObj', JSON.stringify(fileTypesObj));
+      localStorage.setItem("fileTypesObj", JSON.stringify(fileTypesObj));
 
-    setFileTypes(fileTypesObj);
-    setLoading(false);
-  };
+      setFileTypes(fileTypesObj);
+      setLoading(false);
+    };
 
-  fetchFileTypes();
-}, [NFTData]);
+    fetchFileTypes();
+  }, [NFTData]);
 
-const RenderDefault = () => (
-  <Image
-    src={images.invalidImage}
-    alt="NFT"
-    width={350}
-    height={300}
-    objectFit="cover"
-    className={Style.NFTCardTwo_box_img_img}
-    controls
-  />
-);
-
-
-const RenderMedia = ({ src }) => {
-  const isImage = fileTypes[src] && fileTypes[src].startsWith("image");
-  const isAudio = fileTypes[src] && fileTypes[src].startsWith("audio");
-
-  return (
-    <LazyLoadComponent>
-  {isImage ? (
-    <LazyLoadImage
-      src={src}
+  const RenderDefault = () => (
+    <Image
+      src={images.invalidImage}
       alt="NFT"
       width={350}
       height={300}
-      effect="blur"
+      objectFit="cover"
       className={Style.NFTCardTwo_box_img_img}
-    />
-  ) : isAudio ? (
-    <div className={Style.NFTCardTwo_box_audio}>
-      <Image
-        src={images.audio_image}
-        alt="Default"
-        width={350}
-        height={255}
-        objectFit="cover"
-        className={Style.NFTCardTwo_box_img_audio}
-      />
-      <audio
-        src={src}
-        controls
-        className={Style.NFTCardTwo_box_audio_controls}
-      />
-    </div>
-  ) : (
-    <ReactPlayer 
-      url={src}
       controls
-      width='350px'
-      height='300px'
-      className={Style.NFTCardTwo_box_img_img}
     />
-  )}
-</LazyLoadComponent>
   );
-};
 
+  const RenderMedia = ({ src }) => {
+    const isImage = fileTypes[src] && fileTypes[src].startsWith("image");
+    const isAudio = fileTypes[src] && fileTypes[src].startsWith("audio");
 
-const renderFilePreview = (el) => {
-  const fileType = fileTypes[el.image];
+    return (
+      <LazyLoadComponent>
+        {isImage ? (
+          <LazyLoadImage
+            src={src}
+            alt="NFT"
+            width={350}
+            height={300}
+            effect="blur"
+            className={Style.NFTCardTwo_box_img_img}
+          />
+        ) : isAudio ? (
+          <div className={Style.NFTCardTwo_box_audio}>
+            <Image
+              src={images.audio_image}
+              alt="Default"
+              width={350}
+              height={255}
+              objectFit="cover"
+              className={Style.NFTCardTwo_box_img_audio}
+            />
+            <audio
+              src={src}
+              controls
+              className={Style.NFTCardTwo_box_audio_controls}
+            />
+          </div>
+        ) : (
+          <ReactPlayer
+            url={src}
+            controls
+            width="350px"
+            height="300px"
+            className={Style.NFTCardTwo_box_img_img}
+          />
+        )}
+      </LazyLoadComponent>
+    );
+  };
 
-  if (fileType) {
-    return <RenderMedia src={el.image} />;
-  } else {
-    return <RenderDefault />;
-  }
-};
+  const renderFilePreview = (el) => {
+    const fileType = fileTypes[el.image];
 
+    if (fileType) {
+      return <RenderMedia src={el.image} />;
+    } else {
+      return <RenderDefault />;
+    }
+  };
 
   const indexOfLastItem = currentPage * ITEMS_PER_PAGE;
-  const indexOfFirstItem = indexOfLastItem - ITEMS_PER_PAGE;  
+  const indexOfFirstItem = indexOfLastItem - ITEMS_PER_PAGE;
   const currentItems = NFTData.slice(indexOfFirstItem, indexOfLastItem);
 
   return (
@@ -333,11 +256,10 @@ const renderFilePreview = (el) => {
                     <div className={Style.NFTCardTwo_box_info_left}>
                       <p>{el.name}</p>
                     </div>
-                   
                   </div>
-                    <div className={Style.NFTCardTwo_box_tokenid}>
+                  <div className={Style.NFTCardTwo_box_tokenid}>
                     <small> # {el.tokenId}</small>
-                    </div>
+                  </div>
 
                   <div className={Style.NFTCardTwo_box_price}>
                     <div className={Style.likesContainer}>
@@ -345,11 +267,11 @@ const renderFilePreview = (el) => {
                     </div>
                     <div className={Style.NFTCardTwo_box_price_box}>
                       <small>CURRENT PRICE</small>
-                      <p>
-                        {(parseFloat(el.price) * 10 ** 9).toFixed(4)} BNB
-                      </p>
+                      <p>{(parseFloat(el.price) * 10 ** 9).toFixed(4)} BNB</p>
                     </div>
                     <p className={Style.NFTCardTwo_box_price_stock}>
+                      
+                      
                       {/* future auciton functionality 
                     <MdTimer /> <span>{i + 1} HOURS LEFT</span>
                     */}
@@ -366,18 +288,26 @@ const renderFilePreview = (el) => {
                         }
                       />
                       */}
-                      
+
+
                       <Rating
-  emptySymbol={<FaRegStar style={{ marginRight: "5px" }} />}
-  fullSymbol={<FaStar style={{ marginRight: "5px" }} />}
-  onClick={(value) => likeNFT(el.tokenId, value, walletAddress)}
-  initialRating={likes[el.tokenId] ? likes[el.tokenId].rating : 0}
-/>
+                        emptySymbol={
+                          <FaRegStar style={{ marginRight: "5px" }} />
+                        }
+                        fullSymbol={<FaStar style={{ marginRight: "5px" }} />}
+                        onClick={(value) =>
+                          likeNFT(el.tokenId, value, walletAddress)
+                        }
+                        initialRating={
+                          likes[el.tokenId] ? likes[el.tokenId].rating : 0
+                        }
+                      />
 
-<div className={Style.likesNumber}>
-  <span>{likes[el.tokenId] ? likes[el.tokenId].count : 0}</span>
-</div>
-
+                      <div className={Style.likesNumber}>
+                        <span>
+                          {likes[el.tokenId] ? likes[el.tokenId].count : 0}
+                        </span>
+                      </div>
 
                       <div className={Style.likesNumber}>
                         <span>
